6e676d89fa96771d7d573b93b527c16510bc1c21
Nam Trinh <464560@student.saxion.nl>
2019-02-26 11:33:34 +0100

    sampleCodeAdded

--- robotssrc/nl/saxion/FirstRobo.java
+++ robotssrc/nl/saxion/FirstRobo.java
@@ -1,12 +1,18 @@
 package nl.saxion;
 
 import robocode.Robot;
+import robocode.ScannedRobotEvent;
 
 public class FirstRobo extends Robot {
-    public void run(){
-        while (true){
-            ahead(100);
-            turnLeft(100);
+    public void onScannedRobot(ScannedRobotEvent event) {
+        boolean found = false;
+        while (!found){
+            turnRight(1);
+        }
+        if (event.getDistance() < 100){
+            fire(10);
+        } else {
+            fire(0.2);
         }
     }
 }
